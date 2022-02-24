23bbed363a695a5f1ad78cca3369b17ed13c854a
Felicia Ebikon <feliciaebikon@gmail.com>
2022-02-24 12:05:09 +0100

    add payment route

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
+
+
 
 
 
@@ -23,6 +27,10 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+
+
 
 
 
